f6d248181bbe0db29368f673c690e9f88e50c3b8
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-11 20:48:36 -0500

    Update nextGreaterElement.py

--- nextGreaterElement.py
+++ nextGreaterElement.py
@@ -25,3 +25,15 @@ class Solution(object):
         
         # O(n + m)
         num1Ind = {n:i for i,n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+        stack = []
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                ind = num1Ind[val]
+                res[ind] = cur
+            if cur in num1Ind:
+                stack.append(cur)
+
+        return res
